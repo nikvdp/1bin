45a8e883f1d28e6ead797cee407f92109ca57eb6
Nik V <github@nikvdp.com>
2022-01-13 01:12:36 +0800

    rework build script

--- build-all.sh
+++ build-all.sh
@@ -1,25 +1,58 @@
 #!/usr/bin/env bash
-set -eu
-set -x
+# set -eu
+# set -x
 
 # this script should be run by ci to build all the pkgs
 
-./build.sh tree
-./build.sh pv
-./build.sh wget
-./build.sh pandoc
-./build.sh nvim
-./build.sh rsync
-./build.sh rg
-./build.sh curl
-./build.sh tmux
-./build.sh bat
-./build.sh direnv
-./build.sh docker-compose
-./build.sh jc
-./build.sh jq
-./build.sh fzf
-./build.sh git
-CMD_TO_RUN=nvim ./build.sh neovim
-CMD_TO_RUN=rg ./build.sh ripgrep
+simple=(
+    curl
+    wget
+    tree
+    bat
+    pv
+    pandoc
+    fzf
+    jq
+    rsync
+    direnv
+    docker-compose
+    git
+    # jc
+    tmux
+    nodejs
+    socat
+    nc
+    # yq
+    # pstree # non-existent
+)
 
+declare -A complex
+complex[rg]=ripgrep
+complex[node]=nodejs
+complex[npm]=nodejs
+complex[nvim]=neovim
+complex[ssh]=openssh
+complex[sshd]=openssh
+complex[nc]=netcat
+
+
+for cmd_name in "${!complex[@]}"; do 
+    if ! [[ -e "out/$cmd_name" ]]; then
+        export CMD_TO_RUN="$cmd_name"
+        PACKAGE_NAME="${complex[$cmd_name]}"
+        echo "Building $cmd_name (package: $PACKAGE_NAME)"
+
+        ./build.sh "$PACKAGE_NAME" &> "$cmd_name".log
+    else 
+        echo "Skipping $cmd_name"
+    fi
+done
+
+for i in "${simple[@]}"; do
+    if [[ -e "out/$i" ]]; then 
+        echo "Skipping $i"
+    else 
+        echo "Building $i..."
+        ./build.sh "$i" &> "$i".log
+    fi
+done

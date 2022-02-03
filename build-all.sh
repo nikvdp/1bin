57cf5c6a222b41937b78fc190bd8529d4c7da769
Nik V <github@nikvdp.com>
2022-02-03 16:18:30 +0800

    TEMP: disable most of the files for speed

--- build-all.sh
+++ build-all.sh
@@ -5,37 +5,37 @@
 # this script should be run by ci to build all the pkgs
 
 simple=(
-    curl
+    # curl
     wget
-    tree
-    bat
-    pv
-    pandoc
-    fzf
-    jq
-    rsync
-    direnv
-    docker-compose
-    git
-    openssl
-    # jc
-    yarn
-    tmux
-    tmate
-    socat
+    # tree
+    # bat
+    # pv
+    # pandoc
+    # fzf
+    # jq
+    # rsync
+    # direnv
+    # docker-compose
+    # git
+    # openssl
+    # # jc
+    # yarn
+    # tmux
+    # tmate
+    # socat
     # yq
     # pstree # no conda package
 )
 
 declare -A complex
 complex[ag]=the_silver_searcher
-complex[rg]=ripgrep
-complex[node]=nodejs
-complex[npm]=nodejs
-complex[nvim]=neovim
-complex[ssh]=openssh
-complex[sshd]=openssh
-complex[java]=openjdk
+# complex[rg]=ripgrep
+# complex[node]=nodejs
+# complex[npm]=nodejs
+# complex[nvim]=neovim
+# complex[ssh]=openssh
+# complex[sshd]=openssh
+# complex[java]=openjdk
 
 for cmd_name in "${!complex[@]}"; do
     if ! [[ -e "out/$cmd_name" ]]; then
